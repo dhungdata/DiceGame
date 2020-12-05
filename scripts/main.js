4442ea6d0256978e7fff8956173a02fc6fb65189
unknown <dhdata@gmx.com>
2020-12-05 15:19:46 -0800

    added a function to switch players

--- scripts/main.js
+++ scripts/main.js
@@ -1,62 +1,44 @@
-// const p1 = document.getElementById('p-01');
+const p1 = document.getElementById('p-01');
 
-let diceRoll = [0,0,0,0];
+let diceRoll = [0,0];
 let roundScore = [0,0];//first index is user's round score. second index is CPU's round score.
 let totalScore = [0,0];//first index is user's total score. second index is CPU's total score.
 let roundTurn = 0;// 3 rounds total.
 let activePlayer = 0;//0 is user playing. 1 is CPU playing.
 
-function randomizeDice(){
-    if(activePlayer === 0){
-        diceRoll[0] = Math.floor( Math.random() * 6 )+1;
-        diceRoll[1] = Math.floor( Math.random() * 6 )+1;
-    }else{
-        diceRoll[2] = Math.floor( Math.random() * 6 )+1;
-        diceRoll[3] = Math.floor( Math.random() * 6 )+1;
+function switchPlayer(){
+    //switching active player
+    if(activePlayer === 0){//if player is user...
+        activePlayer = 1;  //...then switch to CPU
+    }else{               //else if player is CPU...
+        activePlayer = 0;//... then switch to user
     }
 }
 
+function randomizeDice(){
+    diceRoll[0] = Math.floor( Math.random() * 6 )+1;
+    diceRoll[1] = Math.floor( Math.random() * 6 )+1;
+}
+
 function diceGame(dice1, dice2){
-    if(activePlayer === 0){
-        if(dice1 === 1 || dice2 === 1){
-            roundScore[0] = 0;
-            totalScore[0] += 0;
-            console.log(`P1 - Dice1: ${dice1} Dice2: ${dice2}. Round Score: ${roundScore[0]}`);
-            console.log("P1's Total Score: " + totalScore[0]+"\n");
-            activePlayer = 1;
-        }else if(dice1 === dice2){
-            roundScore[0] = (dice1+dice2)*2;
-            totalScore[0] += (dice1+dice2)*2;
-            console.log(`P1 - (Dice1: ${dice1} Dice2: ${dice2})*2. Round Score: ${roundScore[0]}`);
-            console.log("P1's Total Score: " + totalScore[0]+"\n");
-            activePlayer = 1;
-        }else{
-            roundScore[0] = dice1 + dice2;
-            totalScore[0] += dice1 + dice2;
-            console.log(`P1 - Dice1: ${dice1} Dice2: ${dice2}. Round Score: ${roundScore[0]}`);
-            console.log("P1's Total Score: " + totalScore[0]+"\n");
-            activePlayer = 1;
-        }
+    if(dice1 === 1 || dice2 === 1){
+        roundScore = 0;
+        totalScore[activePlayer] += roundScore;
+        console.log(`Player ${activePlayer} - Dice1: ${dice1} Dice2: ${dice2}. Round Score: ${roundScore}`);
+        console.log(`Player ${activePlayer}'s Total Score: ${totalScore[activePlayer]}\n`);
+        switchPlayer();
+    }else if(dice1 === dice2){
+        roundScore = (dice1+dice2)*2;
+        totalScore[activePlayer] += roundScore;
+        console.log(`Player ${activePlayer} - (Dice1: ${dice1} Dice2: ${dice2})*2. Round Score: ${roundScore}`);
+        console.log(`Player ${activePlayer}'s Total Score: ${totalScore[activePlayer]}\n`);
+        switchPlayer();
     }else{
-        if(dice1 === 1 || dice2 === 1){
-            roundScore[1] = 0;
-            totalScore[1] += o;
-            console.log(`P2 - Dice1: ${dice1} Dice2: ${dice2}. Round Score: ${roundScore[1]}`);
-            console.log("P2's Total Score: " + totalScore[1]+"\n");
-            activePlayer = 0;
-        }else if(dice1 === dice2){
-            roundScore[1] = (dice1+dice2)*2;
-            totalScore[1] += (dice1+dice2)*2;
-            console.log(`P2 - (Dice1: ${dice1} Dice2: ${dice2})*2. Round Score: ${roundScore[1]}`);
-            console.log("P2's Total Score: " + totalScore[1]+"\n");
-            activePlayer = 0;
-        }else{
-            roundScore[1] = dice1 + dice2;
-            totalScore[1] += dice1 + dice2;
-            console.log(`P2 - Dice1: ${dice1} Dice2: ${dice2}. Round Score: ${roundScore[1]}`);
-            console.log("P2's Total Score: " + totalScore[1]+"\n");
-            activePlayer = 0;
-        }
+        roundScore = dice1 + dice2;
+        totalScore[activePlayer] += roundScore;
+        console.log(`Player ${activePlayer} - Dice1: ${dice1} Dice2: ${dice2}. Round Score: ${roundScore}`);
+        console.log(`Player ${activePlayer}'s Total Score: ${totalScore[activePlayer]}\n`);
+        switchPlayer();
     }
 }
 
@@ -64,17 +46,18 @@ const rollDice = document.getElementById("btn-rollDice");
 rollDice.addEventListener('click', function(){
 });
 function play(){//click 'Roll Dice' button to play.
-    activePlayer = 0;
+    //User Roll Dice
     randomizeDice();
     diceGame(diceRoll[0], diceRoll[1]);
     
-    activePlayer = 1;
+    //CPU Roll Dice
     randomizeDice();
-    diceGame(diceRoll[2], diceRoll[3]);
+    diceGame(diceRoll[0], diceRoll[1]);
 
     roundTurn += 1;
+    
     if(roundTurn === 3){
-        console.log(`\nGame Over`);
+        console.log(`Game Over`);
         if(totalScore[0] > totalScore[1]){
             console.log(`${totalScore[0]} > ${totalScore[1]}`);
             console.log("Congratulations! You win!\n");
@@ -88,12 +71,13 @@ function play(){//click 'Roll Dice' button to play.
     }
 }
 
-play();play();play();
+// new game button; Reset everything.
+const newGame = document.getElementById("btn-newGame");
+newGame.addEventListener('click', function(){
+    totalScore = [0,0]
+    roundScore = 0;
+    activePlayer = 0;
 
-// // new game button; Reset everything.
-// const newGame = document.getElementById("btn-newGame");
-// newGame.addEventListener('click', function(){
-//     totalScore = [0,0]
-//     roundScore = 0;
-//     activePlayer = 0;
-// });
+});
+
+play();play();play();
